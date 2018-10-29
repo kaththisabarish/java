ea0b338f90d441d7c7fc5e4021db0c53f4e30308
kaththisabarish <43901464+kaththisabarish@users.noreply.github.com>
2018-10-29 14:29:26 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Fact
+{
+   public static void main(String args[])
+   {
+       int num, i, fact=1;
+       Scanner scan = new Scanner(System.in);
+       num = scan.nextInt();
+
+       for(i=num; i>0; i--)
+       {
+           fact = fact*i;
+       }
+
+       System.out.print(fact);
+   }
+}
