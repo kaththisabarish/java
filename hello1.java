b2e3df025c79174e400c2144223928c8c7562ace
kaththisabarish <43901464+kaththisabarish@users.noreply.github.com>
2019-02-12 15:46:03 +0530

    Add files via upload

---
+++ hello1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class hello
+
+{
+	
+public static void main(String[] args) 
+{
+	    
+int i,n;
+	    
+String str ="hello1";
+		
+System.out.println("Enter the input");
+		
+Scanner in = new Scanner(System.in);
+	    
+n = in.nextInt();
+	    
+for (i=1;i<=n ;i++ )
+	    
+System.out.println("Hello");
+	
+}
+}
